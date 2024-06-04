0c43eebfdce7732d00435f22b8627de280ffb72a
chw970320 <chw970320@gmail.com>
2024-06-04 22:18:38 +0900

    Chore: 설정 변경  - 외부 URL 접근 테스트 및 패키지 구조를 감안한 소스 변경

--- src/main/java/egovframework/example/sample/web/MariaTestController.java
+++ src/main/java/egovframework/example/sample/web/MariaTestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import egovframework.example.sample.service.EgovSampleService;
 import egovframework.example.sample.service.MariaTestVO;
 
 @RestController
+@RequestMapping("/api")
 public class MariaTestController {
 	
 	@Autowired
@@ -19,16 +21,24 @@ public class MariaTestController {
 	
 	/**
 	 * MariaDB 연결 테스트
+	 * @return 샘플 json
 	 */
-	@GetMapping("/api")
+	@GetMapping("/test")
 	public ResponseEntity<?> apiMethodTest() {
 		List<MariaTestVO> list = egovSampleService.selectMariaTest();
 		
 		return ResponseEntity.ok(list);
 	}
 	
-	@GetMapping("/api/test")
-	public String test() {
-		return "test";
+	/**
+	 * TEST API
+	 * @param name
+	 * @return 인삿말
+	 */
+	@GetMapping("/test/{name}")
+	public String test(@PathVariable String name) {
+		StringBuilder greetingMsg = new StringBuilder("Hello, ");
+		
+		return greetingMsg.append(name).toString();
 	}
 }
