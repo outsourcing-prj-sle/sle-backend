41487e8a7c10c2caefc25885a007f299c397576c
mikohatsu <allenhong36@gmail.com>
2024-06-12 22:22:59 +0900

    1
    1

---
+++ src/main/java/egovframework/example/cmmn/service/AccountVO.java
@@ -0,0 +1,14 @@
+package egovframework.example.cmmn.service;
+
+public class AccountVO {
+	
+	/**
+	 * 사용자 id
+	 */
+	private String id;
+	/**
+	 * 사용자 password
+	 */
+	private String password;
+
+}
