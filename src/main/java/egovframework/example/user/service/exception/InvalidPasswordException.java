6103b3c5a9e517accc68408ff94ce0e98cdb638e
mikohatsu <allenhong36@gmail.com>
2024-06-16 02:55:20 +0900

    delete
    1

--- src/main/java/egovframework/example/user/service/exception/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package egovframework.example.user.service.exception;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
--- src/main/java/egovframework/example/user/service/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package egovframework.example.user.service.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
